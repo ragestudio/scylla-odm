f96e3709b2e74356f710ae7f7ce011d1a1e8841f
srgooglo <srgooglo@ragestudio.net>
2026-05-07 17:31:07 +0200

    Update query parsing to use queryParser utility

--- src/operations/update.ts
+++ src/operations/update.ts
@@ -1,11 +1,13 @@
 import type Model from "../model"
 import fillDefaults from "../utils/fillDefaults"
+import queryParser from "../utils/queryParser"
 
 export default async function updateOP<TDoc>(
 	this: Model<any, TDoc>,
 	query: any,
 ) {
 	query = fillDefaults(this.schema, query)
+	query = queryParser(this, query)
 
 	if (typeof query.__v !== "undefined") {
 		if (Number.isNaN(query.__v)) {
